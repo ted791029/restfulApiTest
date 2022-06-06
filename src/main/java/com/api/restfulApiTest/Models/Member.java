fe9bdb02860ac2e0e435a5971503d96690e40227
DESKTOP-93NG053\azooc <azoocx791029@gmai.com>
2022-06-06 16:45:25 +0800

    1.修改格式

--- src/main/java/com/api/restfulApiTest/Models/Member.java
+++ src/main/java/com/api/restfulApiTest/Models/Member.java
@@ -5,7 +5,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "pa_member")
+@Table(name = "PA_MEMBER", schema = "public")
 public class Member {
 	/**唯一碼**/
 	@Id
